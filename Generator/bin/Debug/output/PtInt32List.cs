0e8c9ab0f67edc91964e998c951a9c59a6996e1a
Haijie HJ2 Pan <panhj2@lenovo.com>
2021-09-29 11:47:55 +0800

    format

--- Generator/bin/Debug/output/PtInt32List.cs
+++ Generator/bin/Debug/output/PtInt32List.cs
@@ -6,37 +6,37 @@ using Protocol.Net;
 
 namespace Development.Net.Pt
 {
-public class PtInt32List
-{
-    public byte __tag__ { get;private set;}
-
-	public List<int> Elements{ get;private set;}
-	   
-    public PtInt32List SetElements(List<int> value){Elements=value; __tag__|=1; return this;}
-	
-    public bool HasElements(){return (__tag__&1)==1;}
-	
-    public static byte[] Write(PtInt32List data)
+    public class PtInt32List
     {
-        using(ByteBuffer buffer = new ByteBuffer())
+        public byte __tag__ { get; private set; }
+
+        public List<int> Elements { get; private set; }
+
+        public PtInt32List SetElements(List<int> value) { Elements = value; __tag__ |= 1; return this; }
+
+        public bool HasElements() { return (__tag__ & 1) == 1; }
+
+        public static byte[] Write(PtInt32List data)
         {
-            buffer.WriteByte(data.__tag__);
-			if(data.HasElements())buffer.WriteCollection(data.Elements,element=>buffer.WriteInt32(element));
-			
-            return buffer.GetRawBytes();
+            using (ByteBuffer buffer = new ByteBuffer())
+            {
+                buffer.WriteByte(data.__tag__);
+                if (data.HasElements()) buffer.WriteCollection(data.Elements, element => buffer.WriteInt32(element));
+
+                return buffer.GetRawBytes();
+            }
+        }
+
+        public static PtInt32List Read(byte[] bytes)
+        {
+            using (ByteBuffer buffer = new ByteBuffer(bytes))
+            {
+                PtInt32List data = new PtInt32List();
+                data.__tag__ = buffer.ReadByte();
+                if (data.HasElements()) data.Elements = buffer.ReadCollection(() => buffer.ReadInt32());
+
+                return data;
+            }
         }
     }
-
-    public static PtInt32List Read(byte[] bytes)
-    {
-        using(ByteBuffer buffer = new ByteBuffer(bytes))
-        {
-            PtInt32List data = new PtInt32List();
-            data.__tag__ = buffer.ReadByte();
-			if(data.HasElements())data.Elements = buffer.ReadCollection(()=>buffer.ReadInt32());
-			
-            return data;
-        }       
-    }
-}
 }
--- Generator/bin/Debug/output/PtRoomList.cs
+++ Generator/bin/Debug/output/PtRoomList.cs
@@ -6,37 +6,37 @@ using Protocol.Net;
 
 namespace Development.Net.Pt
 {
-public class PtRoomList
-{
-    public byte __tag__ { get;private set;}
-
-	public List<PtRoom> Rooms{ get;private set;}
-	   
-    public PtRoomList SetRooms(List<PtRoom> value){Rooms=value; __tag__|=1; return this;}
-	
-    public bool HasRooms(){return (__tag__&1)==1;}
-	
-    public static byte[] Write(PtRoomList data)
+    public class PtRoomList
     {
-        using(ByteBuffer buffer = new ByteBuffer())
+        public byte __tag__ { get; private set; }
+
+        public List<PtRoom> Rooms { get; private set; }
+
+        public PtRoomList SetRooms(List<PtRoom> value) { Rooms = value; __tag__ |= 1; return this; }
+
+        public bool HasRooms() { return (__tag__ & 1) == 1; }
+
+        public static byte[] Write(PtRoomList data)
         {
-            buffer.WriteByte(data.__tag__);
-			if(data.HasRooms())buffer.WriteCollection(data.Rooms,element=>PtRoom.Write(element));
-			
-            return buffer.GetRawBytes();
+            using (ByteBuffer buffer = new ByteBuffer())
+            {
+                buffer.WriteByte(data.__tag__);
+                if (data.HasRooms()) buffer.WriteCollection(data.Rooms, element => PtRoom.Write(element));
+
+                return buffer.GetRawBytes();
+            }
+        }
+
+        public static PtRoomList Read(byte[] bytes)
+        {
+            using (ByteBuffer buffer = new ByteBuffer(bytes))
+            {
+                PtRoomList data = new PtRoomList();
+                data.__tag__ = buffer.ReadByte();
+                if (data.HasRooms()) data.Rooms = buffer.ReadCollection(retbytes => PtRoom.Read(retbytes));
+
+                return data;
+            }
         }
     }
-
-    public static PtRoomList Read(byte[] bytes)
-    {
-        using(ByteBuffer buffer = new ByteBuffer(bytes))
-        {
-            PtRoomList data = new PtRoomList();
-            data.__tag__ = buffer.ReadByte();
-			if(data.HasRooms())data.Rooms = buffer.ReadCollection(retbytes=>PtRoom.Read(retbytes));
-			
-            return data;
-        }       
-    }
-}
 }
